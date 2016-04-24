baba1f0dddbc4c9917335f10dbc65eaeb71e95c5
Diego Terán <diegoteran94@gmail.com>
2016-04-24 17:53:46 -0500

    Added E

--- Codeforces/348Div2/E.cpp
+++ Codeforces/348Div2/E.cpp
@@ -11,8 +11,14 @@ typedef tree<pii, null_type, less<pii>, rb_tree_tag, tree_order_statistics_node_
 #define MAXN 1000010
 #define mp make_pair
 
-ordered_set t[MAXN];
-map<int, int> m;
+struct query{
+	int a, t, x;
+};
+
+query q[MAXN];
+pii compress[MAXN];
+ordered_set t[MAXN], t2[MAXN];
+map<int, int> m, m2;
 
 void updateAdd(int idx, int s){
 	m[s]++;
@@ -21,15 +27,16 @@ void updateAdd(int idx, int s){
 }
 
 void updateSub(int idx, int s){
+	m2[s]++;
 	for (; idx < MAXN; idx += (idx&-idx))
-		t[idx].erase(mp(s, m[s]));
-	m[s]--;
+		t2[idx].insert(mp(s, m2[s]));
 }
 
 int query(int idx, int s){
 	int sum = 0;
-	for(; idx; idx -= (idx&-idx))
-		sum += t[idx].order_of_key(mp(s+1, 0));
+	for(; idx; idx -= (idx&-idx)){
+		sum += (t[idx].order_of_key(mp(s+1, 0)) - t2[idx].order_of_key(mp(s+1,0)));
+	}
 	return sum;
 }
 
@@ -38,16 +45,23 @@ int N, a, ti, x;
 int main(){
 
 	scanf("%d", &N);
-	while (N--){
+	for (int i = 0; i < N; i++){		
+		scanf("%d %d %d", &q[i].a, &q[i].t, &q[i].x);
+		compress[i] = mp(q[i].t, i);
+	}
+	sort(compress, compress+N);
+
+	for (int i = 0; i < N; i++)
+		q[compress[i].second].t = i+1;
+	
+	for (int i = 0; i < N; i++){
+		if(q[i].a == 1)
+			updateAdd(q[i].t, q[i].x);
 		
-		scanf("%d %d %d", &a, &ti, &x);
-		if(a == 1)
-			updateAdd(ti, x);
+		if(q[i].a == 2)
+			updateSub(q[i].t, q[i].x);
 		
-		if(a == 2)
-			updateSub(ti, x);
-		
-		if(a == 3)
-			printf("%d\n", query(ti, x) - query(ti, x-1));
+		if(q[i].a == 3)
+			printf("%d\n", query(q[i].t, q[i].x) - query(q[i].t, q[i].x-1));
 	}
 }
