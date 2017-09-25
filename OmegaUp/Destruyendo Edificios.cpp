d19bebf0eb16454a9248afd261bf164df32997d3
diegoteran <diegoteran94@gmail.com>
2017-09-25 13:59:03 -0500

    OmegaUp modification and others

--- OmegaUp/Destruyendo Edificios.cpp
+++ OmegaUp/Destruyendo Edificios.cpp
@@ -1,29 +1,37 @@
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
-#define MAXN 1000005
+#define MAXN 1000100
 
-long long N, i, j, b[MAXN], lastLeft[MAXN], lastRight[MAXN], ans;
+long long ar[MAXN], l[MAXN], r[MAXN], N;
 
 int main(){
-	scanf("%lld", &N);
-	for (i = 1; i <= N; i++)
-		scanf("%lld", &b[i]);
-	b[0] = -1; b[N+1] = -1;
+
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		scanf("%lld", ar + i);
 	
-	//All nearest smaller value(both sides)
-	fill(lastRight, lastRight+N+2, N+1);
-	for (i = 1; i <= N; i++){
-		for (j = i-1; b[j] >= b[i]; j = lastLeft[j]);
-		lastLeft[i] = j;
-		for (j = (N-i+2); b[j] >= b[N-i+1]; j = lastRight[j]);
-		lastRight[N-i+1] = j;
+	ar[0] = ar[N+1] = -1;
+	for (int i = 1; i <= N; i++) {
+		int j = i-1;
+		while (ar[j] >= ar[i])
+			j = l[j];
+		l[i] = j;
 	}
-	ans = 0;
-	for (i = 1; i <= N; i++)
-		ans = max(ans, b[i]*(lastRight[i] - lastLeft[i] - 1));
-	
-	printf("%lld\n", ans);
+
+	for (int i = N; i > 0; i--) {
+		int j = i+1;
+		while (ar[j] >= ar[i])
+			j = r[j];
+		r[i] = j;
+	}
+
+	long long M = 0;
+	for (int i = 1; i <= N; i++)
+		M = max(M, (r[i] - l[i] - 1)*ar[i]);
+
+	cout << M << endl;
 }
